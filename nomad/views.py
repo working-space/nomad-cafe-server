f5c37aa09049e50b924d3b5c98af04c43a1650ee
ghkdxofla <ghkdxofla@gmail.com>
2020-08-09 11:37:38 +0000

    parameter 검색 기능 추가

--- nomad/views.py
+++ nomad/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from .models import Cafe
 from rest_framework import viewsets
+from rest_framework.response import Response
 from nomad.serializers import UserSerializer, CafeSerializer
 # from django.shortcuts import render
 
@@ -12,7 +13,20 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-    
-class CafeViewSet(viewsets.ModelViewSet):
-    queryset = Cafe.objects.all()
+
+class CafeViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Cafe.objects.all() 
     serializer_class = CafeSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        address = self.request.query_params.get('address', None)
+        _id = self.request.query_params.get('id', None)
+        if _id is not None:
+            queryset = queryset.filter(_id=_id)
+        if address is not None:
+            queryset = queryset.filter(road_addr=address)
+        return queryset
--- was/urls.py
+++ was/urls.py
@@ -23,7 +23,6 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'cafes', views.CafeViewSet)
 
-
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
