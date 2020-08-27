365177c879001fd0412006eadcdfcbb3fa15dfa5
ghkdxofla <ghkdxofla@gmail.com>
2020-08-27 11:37:48 +0000

    rollback previous version of settings.py

--- was/settings.py
+++ was/settings.py
@@ -58,7 +58,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'was.urls'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'PAGE_SIZE': 10
 }
